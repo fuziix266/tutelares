1f0f3117514d0bd60564213e1669bcb5b2722739
Drako-CL <fsantander.informatico@gmail.com>
2026-03-28 00:37:52 -0300

    Allow .env to override environment variables

--- public/index.php
+++ public/index.php
@@ -28,11 +28,9 @@ if (file_exists(__DIR__ . '/../.env')) {
         list($name, $value) = explode('=', $line, 2);
         $name = trim($name);
         $value = trim($value);
-        if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
-            putenv(sprintf('%s=%s', $name, $value));
-            $_ENV[$name] = $value;
-            $_SERVER[$name] = $value;
-        }
+        putenv(sprintf('%s=%s', $name, $value));
+        $_ENV[$name] = $value;
+        $_SERVER[$name] = $value;
     }
 }
 
